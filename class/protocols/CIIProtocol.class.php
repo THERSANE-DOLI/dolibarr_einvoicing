0b7d80397eadc8c97965dd73d17c8f390df4182b
Mallory <mallo.guyot@free.fr>
2026-07-03 21:17:23 +0200

    FIX: invoice date always null

--- class/protocols/CIIProtocol.class.php
+++ class/protocols/CIIProtocol.class.php
@@ -574,7 +574,7 @@ class CIIProtocol extends AbstractProtocol
 		if ($supplierInvoice->type === '-1') {
 			return ['res' => -1, 'message' => 'Unfounded dolibarr corresponding Invoice code for document type code: ' . ($parsedHeader['documenttypecode'] ?? 'NA')];
 		}
-		$supplierInvoice->date = isset($parsedHeader['documentdate']) && $parsedHeader['documentdate'] instanceof DateTime ? $parsedHeader['documentdate']->format('Y-m-d') : null;
+		$supplierInvoice->date = !empty($parsedHeader['documentdate']) ? dol_stringtotime($parsedHeader['documentdate']) : null;
 
 
 		// Set currency
