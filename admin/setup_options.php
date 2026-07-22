d155aadc217f2bbe1bd429f1a9810608447dee2c
Laurent Destailleur <eldy@destailleur.fr>
2026-07-22 13:14:09 +0200

    Hide a useless.Should be always on. Disabling it is possible by editing hidden cosntant

--- admin/setup_options.php
+++ admin/setup_options.php
@@ -334,19 +334,21 @@ if (!getDolGlobalString('EINVOICING_DISABLE_SYNC_AP_TO_DOLI')) {
 	$item->cssClass = 'minwidth500';
 
 	// Setup conf to to enable a limit of flows to synchronize per one synchronization call
+	/* This option is useless, should be always on. Disabling it is possible by editing hidden cosntant
 	$item = $formSetup->newItem('EINVOICING_FLOWS_SYNC_CALL_LIMIT')->setAsYesNo();
 	$item->helpText = $langs->transnoentities('EINVOICING_FLOWS_SYNC_CALL_LIMIT_HELP');
 	$item->defaultFieldValue = '1';
 	$item->cssClass = 'minwidth500';
 	$item->fieldParams['forcereload'] = 1;
+	*/
 
-	if (getDolGlobalString('EINVOICING_FLOWS_SYNC_CALL_LIMIT')) {
+	//if (getDolGlobalString('EINVOICING_FLOWS_SYNC_CALL_LIMIT')) {
 		// Setup conf to to define the number of flows to synchronize per one synchronization call
 		$item = $formSetup->newItem('EINVOICING_FLOWS_SYNC_CALL_SIZE');
 		$item->helpText = $langs->transnoentities('EINVOICING_FLOWS_SYNC_CALL_SIZE_HELP');
 		$item->defaultFieldValue = '100';
 		$item->cssClass = 'maxwidth100';
-	}
+	//}
 
 	// Setup conf to define a time margin in hours to go back from the current date of the last synchronization
 	$item = $formSetup->newItem('EINVOICING_SYNC_MARGIN_TIME_HOURS');
