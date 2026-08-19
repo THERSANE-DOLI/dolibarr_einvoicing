a2331ff743add221a6d564b4f33648b9980b050a
Laurent Destailleur <eldy@destailleur.fr>
2026-08-19 23:07:17 +0200

    The message must remain on top, so we use warning.

--- admin/setup.php
+++ admin/setup.php
@@ -268,7 +268,9 @@ if ($prefix && preg_match('/call'.$prefix.'HEALTHCHECK/i', $action, $reg)) {
 // Action show remote info
 if ($prefix && preg_match('/call'.$prefix.'REMOTEINFO/i', $action, $reg)) {
 	$remoteInfo = $provider->getRemoteInfo();
-	setEventMessages($remoteInfo['message'], null, ($remoteInfo['status_code'] == 200 ? 'mesgs' : 'warnings'));
+	//$style = ($remoteInfo['status_code'] == 200 ? 'warnings' : 'errors');
+	$style = 'warnings';	// We always use warnings because we need message to be persistent (too long to read).
+	setEventMessages($remoteInfo['message'], null, $style);
 }
 
 // Generate a sample invoice and try to send it
