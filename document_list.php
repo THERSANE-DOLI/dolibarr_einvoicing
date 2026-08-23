Dolibarr/dolibarr-community-modules

FIX [einvoicing] Make the flow list page work on Dolibarr 17
einvoicing/document_list.php has never been able to render on Dolibarr 17, the version the descriptor of
the module declares as the minimum it supports. Three things stood in the way.

The page asks CommonObject::getFieldList() to leave 'recap' and 'xml_data' out of the SELECT. That second
argument was added to the core in 18: on 17 the method takes the alias alone and ignores what else it is
given, without a word, so 'recap' - a virtual column of this list, which has no column in the table - went
into the query and the page answered a technical error, "Unknown column 't.recap' in 'SELECT'". Below 18
the page builds the same list the core builds since 18; on 18 and above getFieldList() is still the one
doing it, so nothing changes there.

Past that, the page died on "Call to undefined function GETPOSTDATE()", a helper the core gained in 18 as
well. It is polyfilled in the library of the module next to GETPOSTFLOAT(), identical to the core
implementation, and the page now loads that library - which also covers dolPrintHTMLForAttribute(), a
function of Dolibarr 19 the same page calls to render its text columns and that would fatal there for the
same reason.

Finally, six places of the page read $conf->main_checkbox_left_column directly, a property the core only
carries since Dolibarr 21, which had every load below that write a PHP warning per place for a value that
is empty there anyway. They read it through empty() now, the way a seventh place already did.

Tested on the eight versions the module has to live with, 17.0.4 to 24.0.0-beta: the list renders, sorts,
filters and paginates on 17 with no PHP warning of its own, and the pages of 18 to 24 answer exactly what
they answered before - same rows, same number of SQL queries.

Co-Authored-By: Claude Opus 5 (1M context) <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01JJxfQxnvTp8MmDkUWJJCE7

einvoicing/document_list.php
@@ -78,6 +78,7 @@ include_once __DIR__.'/class/providers/PDPProviderManager.class.php';
 include_once __DIR__.'/class/protocols/ProtocolManager.class.php';
 
 // load module libraries
+include_once __DIR__.'/lib/einvoicing.lib.php';
 include_once __DIR__.'/class/document.class.php';
 // for other modules
 //dol_include_once('/othermodule/class/otherobject.class.php');
@@ -359,7 +360,24 @@ $morecss = array();
 // Build and execute select
 // --------------------------------------------------------------------
 $sql = "SELECT";
-$sql .= " ".$object->getFieldList('t', array('recap', 'xml_data'));
+// Fields of the table for the SELECT. 'recap' is a virtual column of this list - it has no column in
+// the table - and 'xml_data' is never displayed, so both must stay out of the query.
+$fieldstoexcludefromselect = array('recap', 'xml_data');
+if ((float) DOL_VERSION < 18) {
+	// CommonObject::getFieldList() takes what to exclude only since Dolibarr 18: on 17 it takes the alias
+	// alone and ignores the rest without a word, which sends 't.recap' into the query and fails it. Build
+	// the same list as the core builds since 18.
+	$fieldlist = array();
+	foreach (array_keys($object->fields) as $fieldname) {
+		if (in_array($fieldname, $fieldstoexcludefromselect)) {
+			continue;
+		}
+		$fieldlist[] = 't.'.$fieldname;
+	}
+	$sql .= " ".implode(',', $fieldlist);
+} else {
+	$sql .= " ".$object->getFieldList('t', $fieldstoexcludefromselect);
+}
 // Add fields from extrafields
 if (!empty($extrafields->attributes[$object->table_element]['label'])) {
 	foreach ($extrafields->attributes[$object->table_element]['label'] as $key => $val) {
@@ -702,7 +720,7 @@ if (!empty($moreforfilter)) {
 
 $varpage = empty($contextpage) ? $_SERVER["PHP_SELF"] : $contextpage;
 //$varpage = '';
-$htmlofselectarray = $form->multiSelectArrayWithCheckbox('selectedfields', $arrayfields, $varpage, $conf->main_checkbox_left_column);  // This also change content of $arrayfields with user setup
+$htmlofselectarray = $form->multiSelectArrayWithCheckbox('selectedfields', $arrayfields, $varpage, empty($conf->main_checkbox_left_column) ? 0 : $conf->main_checkbox_left_column);  // This also change content of $arrayfields with user setup
 $selectedfields = (($mode != 'kanban' && $mode != 'kanbangroupby') ? $htmlofselectarray : '');
 $selectedfields .= (count($arrayofmassactions) ? $form->showCheckAddButtons('checkforselect', 1) : '');
 
@@ -1018,7 +1036,7 @@ print '<table class="tagtable nobottomiftotal noborder liste'.($moreforfilter ? 
 // --------------------------------------------------------------------
 print '<tr class="liste_titre_filter">';
 // Action column
-if ($conf->main_checkbox_left_column) {
+if (!empty($conf->main_checkbox_left_column)) {
 	print '<td class="liste_titre center maxwidthsearch">';
 	$searchpicto = $form->showFilterButtons('left');
 	print $searchpicto;
@@ -1079,7 +1097,7 @@ print $hookmanager->resPrint;
 	print '<td class="liste_titre"></td>';
 }*/
 // Action column
-if (!$conf->main_checkbox_left_column) {
+if (empty($conf->main_checkbox_left_column)) {
 	print '<td class="liste_titre center maxwidthsearch">';
 	$searchpicto = $form->showFilterButtons();
 	print $searchpicto;
@@ -1094,7 +1112,7 @@ $totalarray['nbfield'] = 0;
 // --------------------------------------------------------------------
 print '<tr class="liste_titre">';
 // Action column
-if ($conf->main_checkbox_left_column) {
+if (!empty($conf->main_checkbox_left_column)) {
 	print getTitleFieldOfList($selectedfields, 0, $_SERVER["PHP_SELF"], '', '', '', '', $sortfield, $sortorder, 'center maxwidthsearch ')."\n";
 	$totalarray['nbfield']++;
 }
@@ -1126,7 +1144,7 @@ print $hookmanager->resPrint;
 	$totalarray['nbfield']++;
 }*/
 // Action column
-if (!$conf->main_checkbox_left_column) {
+if (empty($conf->main_checkbox_left_column)) {
 	print getTitleFieldOfList($selectedfields, 0, $_SERVER["PHP_SELF"], '', '', '', '', $sortfield, $sortorder, 'center maxwidthsearch ')."\n";
 	$totalarray['nbfield']++;
 }
@@ -1225,7 +1243,7 @@ while ($i < $imaxinloop) {
 		print '<tr data-rowid="'.$object->id.'" class="oddeven row-with-select">';
 
 		// Action column
-		if ($conf->main_checkbox_left_column) {
+		if (!empty($conf->main_checkbox_left_column)) {
 			print '<td class="nowrap center">';
 			if ($massactionbutton || $massaction) { // If we are in select mode (massactionbutton defined) or if we have already selected and sent an action ($massaction) defined
 				$selected = 0;

THERSANE-DOLI/dolibarr_einvoicing

document_list.php
@@ -79,6 +79,11 @@ include_once __DIR__.'/class/providers/PDPProviderManager.class.php';
 include_once __DIR__.'/class/protocols/ProtocolManager.class.php';
 
 // load module libraries
+// GETPOSTDATE() arrived in the core in Dolibarr 18 and dolPrintHTMLForAttribute() in Dolibarr 19; this
+// page calls both, so it would fatal below the version the module declares it supports. The first is
+// backported next to the other core helpers of compat/, the second already ships in the module library.
+include_once __DIR__.'/compat/functions.lib.php';
+include_once __DIR__.'/lib/einvoicing.lib.php';
 include_once __DIR__.'/class/document.class.php';
 // for other modules
 //dol_include_once('/othermodule/class/otherobject.class.php');
@@ -378,7 +383,25 @@ $morecss = array();
 // Build and execute select
 // --------------------------------------------------------------------
 $sql = "SELECT";
-$sql .= " ".$object->getFieldList('t', array('recap', 'xml_data', 'thirdparty'));
+// Fields of the table for the SELECT. 'recap' and 'thirdparty' are virtual columns of this list - they
+// have no column in the table - and 'xml_data' is never displayed, so the three must stay out of the query.
+$columns = array('recap', 'xml_data', 'thirdparty');
+if ((float) DOL_VERSION < 18) {
+	// CommonObject::getFieldList() takes what to exclude only since Dolibarr 18: on 17 it takes the alias
+	// alone and ignores the rest without a word, which sends 't.recap' into the query and fails it. Build
+	// the same list as the core builds since 18.
+	$fields = array();
+	foreach (array_keys($object->fields) as $fieldname) {
+		if (in_array($fieldname, $columns)) {
+			continue;
+		}
+		$fields[] = 't.'.$fieldname;
+	}
+	$selectFields = implode(',', $fields);
+} else {
+	$selectFields = $object->getFieldList('t', $columns);
+}
+$sql .= " ".$selectFields;
 // Add fields from extrafields
 if (!empty($extrafields->attributes[$object->table_element]['label'])) {
 	foreach ($extrafields->attributes[$object->table_element]['label'] as $key => $val) {
@@ -743,7 +766,7 @@ if (!empty($moreforfilter)) {
 
 $varpage = empty($contextpage) ? $_SERVER["PHP_SELF"] : $contextpage;
 //$varpage = '';
-$htmlofselectarray = $form->multiSelectArrayWithCheckbox('selectedfields', $arrayfields, $varpage, $conf->main_checkbox_left_column);  // This also change content of $arrayfields with user setup
+$htmlofselectarray = $form->multiSelectArrayWithCheckbox('selectedfields', $arrayfields, $varpage, empty($conf->main_checkbox_left_column) ? 0 : $conf->main_checkbox_left_column);  // This also change content of $arrayfields with user setup
 $selectedfields = (($mode != 'kanban' && $mode != 'kanbangroupby') ? $htmlofselectarray : '');
 $selectedfields .= (count($arrayofmassactions) ? $form->showCheckAddButtons('checkforselect', 1) : '');
 
@@ -1059,7 +1082,7 @@ print '<table class="tagtable nobottomiftotal noborder liste'.($moreforfilter ? 
 // --------------------------------------------------------------------
 print '<tr class="liste_titre_filter">';
 // Action column
-if ($conf->main_checkbox_left_column) {
+if (!empty($conf->main_checkbox_left_column)) {
 	print '<td class="liste_titre center maxwidthsearch">';
 	$searchpicto = $form->showFilterButtons('left');
 	print $searchpicto;
@@ -1122,7 +1145,7 @@ print $hookmanager->resPrint;
 	print '<td class="liste_titre"></td>';
 }*/
 // Action column
-if (!$conf->main_checkbox_left_column) {
+if (empty($conf->main_checkbox_left_column)) {
 	print '<td class="liste_titre center maxwidthsearch">';
 	$searchpicto = $form->showFilterButtons();
 	print $searchpicto;
@@ -1137,7 +1160,7 @@ $totalarray['nbfield'] = 0;
 // --------------------------------------------------------------------
 print '<tr class="liste_titre">';
 // Action column
-if ($conf->main_checkbox_left_column) {
+if (!empty($conf->main_checkbox_left_column)) {
 	print getTitleFieldOfList($selectedfields, 0, $_SERVER["PHP_SELF"], '', '', '', '', $sortfield, $sortorder, 'center maxwidthsearch ')."\n";
 	$totalarray['nbfield']++;
 }
@@ -1170,7 +1193,7 @@ print $hookmanager->resPrint;
 	$totalarray['nbfield']++;
 }*/
 // Action column
-if (!$conf->main_checkbox_left_column) {
+if (empty($conf->main_checkbox_left_column)) {
 	print getTitleFieldOfList($selectedfields, 0, $_SERVER["PHP_SELF"], '', '', '', '', $sortfield, $sortorder, 'center maxwidthsearch ')."\n";
 	$totalarray['nbfield']++;
 }
@@ -1272,7 +1295,7 @@ while ($i < $imaxinloop) {
 		print '<tr data-rowid="'.$object->id.'" class="oddeven row-with-select">';
 
 		// Action column
-		if ($conf->main_checkbox_left_column) {
+		if (!empty($conf->main_checkbox_left_column)) {
 			print '<td class="nowrap center">';
 			if ($massactionbutton || $massaction) { // If we are in select mode (massactionbutton defined) or if we have already selected and sent an action ($massaction) defined
 				$selected = 0;