bf9b8a143bb1f86439d24580e89c4bae648c1e4c
Laurent Destailleur <eldy@destailleur.fr>
2026-08-29 08:16:02 +0200

    Better message

--- class/providers/EsalinkPDPProvider.class.php
+++ class/providers/EsalinkPDPProvider.class.php
@@ -957,9 +957,6 @@ class EsalinkPDPProvider extends AbstractPDPProvider
 		}
 
 		$results = $response['response']['results'] ?? array();
-		if (empty($results)) {
-			break;
-		}
 
 		// Since AP may not return flows in the order they want (by updatedAt ASC), we sort them here.
 		// On the sub-second key, because the cursor moves along it.
@@ -1055,8 +1052,17 @@ class EsalinkPDPProvider extends AbstractPDPProvider
 						if ($rescode == 'THIRDPARTY_NOT_FOUND') {
 							$infostring = '';
 							foreach ($res['actiondata'] ?? [] as $datakey => $dataval) {
-								if ($datakey && $dataval) {
-									$infostring .= ($infostring ? ', ' : '').$datakey.': '.$dataval;
+								if ($datakey && $dataval && in_array($datakey, array('name', 'email', 'vatnumber', 'idprof1'))) {
+									$transdatakey = ucfirst($datakey);
+									if ($transdatakey == 'Vatnumber') {
+										$transdatakey = 'VATIntraShort';
+									}
+									if ($transdatakey == 'Idprof1') {
+										$transdatakey = 'ProfId1';
+									}
+									$infostring .= ($infostring ? ', ' : '');
+									$infostring .= $langs->transnoentitiesnoconv($transdatakey);
+									$infostring .= ': '.$dataval;
 								}
 							}
 							$actions[$rescode]['businessmessage'] = $langs->trans("CantFindThirdpartyFromTheImportedInvoice", $infostring);
